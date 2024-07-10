e1b07da22861a09d0dcc2042fa804060ae7ef0a2
Alicja Michalska <ahplka19@gmail.com>
2024-07-10 18:45:36 +0200

    mb/erying/tgl: Change PCI-E configuration in ramstage

--- src/mainboard/erying/tgl/ramstage.c
+++ src/mainboard/erying/tgl/ramstage.c
@@ -16,25 +16,6 @@ struct chip_operations mainboard_ops = {
 
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
-	// PEG0 - Gen4 NVME
-	params->CpuPcieRpSlotImplemented[0] = 1;
-	params->CpuPcieRpAdvancedErrorReporting[0] = 1;
-	params->CpuPcieRpPeerToPeerMode[0] = 1;
-	params->CpuPcieRpPtmEnabled[0] = 1;
-	params->CpuPcieRpLtrEnable[0] = 1;
-	params->CpuPcieRpPmSci[0] = 1;
-	params->CpuPcieRpAcsEnabled[0] = 1;
-	params->CpuPcieRpTransmitterHalfSwing[0] = 0;
-
-	// PEG1 - PCI-E x16
-	params->CpuPcieRpSlotImplemented[1] = 1;
-	params->CpuPcieRpAdvancedErrorReporting[1] = 1;
-	params->CpuPcieRpPeerToPeerMode[1] = 1;
-	params->CpuPcieRpPtmEnabled[1] = 1;
-	params->CpuPcieRpLtrEnable[1] = 1;
-	params->CpuPcieRpPmSci[1] = 1;
-	params->CpuPcieRpAcsEnabled[1] = 1;
-	params->CpuPcieRpTransmitterHalfSwing[1] = 0;
 
 	uint8_t aspm, aspm_l1;
 
@@ -51,44 +32,62 @@ void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 		aspm_l1 = 0;
 	}
 
-	// CPU PCI-E ASPM
-	params->CpuPciePowerGating = CONFIG(PCIEXP_CLK_PM);
-	params->CpuPcieClockGating = CONFIG(PCIEXP_CLK_PM);
 
+	// PEG0 - Gen4 NVME
+	params->CpuPcieRpSlotImplemented[0] = 1;
+	params->CpuPcieRpAdvancedErrorReporting[0] = 1;
+	params->CpuPcieRpPeerToPeerMode[0] = 1;
+	params->CpuPcieRpAcsEnabled[0] = 1;
+	params->CpuPcieRpTransmitterHalfSwing[0] = 1;
 	params->CpuPcieRpEnableCpm[0] = CONFIG(PCIEXP_CLK_PM);
 	params->CpuPcieRpAspm[0] = aspm;
 	params->CpuPcieRpL1Substates[0] = aspm_l1;
 
+	// PEG1 - PCI-E x16
+	params->CpuPcieRpSlotImplemented[1] = 1;
+	params->CpuPcieRpAdvancedErrorReporting[1] = 1;
+	params->CpuPcieRpPeerToPeerMode[1] = 1;
+	params->CpuPcieRpAcsEnabled[1] = 1;
+	params->CpuPcieRpTransmitterHalfSwing[1] = 1;
 	params->CpuPcieRpEnableCpm[1] = CONFIG(PCIEXP_CLK_PM);
 	params->CpuPcieRpAspm[1] = aspm;
 	params->CpuPcieRpL1Substates[1] = aspm_l1;
 
-	// Power management - ASPM is broken even on vendor FW
+	// Power management
+	params->CpuPciePowerGating = CONFIG(PCIEXP_CLK_PM);
+	params->CpuPcieClockGating = CONFIG(PCIEXP_CLK_PM);
 	params->PchLegacyIoLowLatency = 1;
 	params->PchDmiAspmCtrl = 0;
 
 	// PCH RootPorts
-	params->PcieRpMaxPayload[4]  = 2; // M.2 Gen3
+	params->PcieRpMaxPayload[4] = 2; // M.2 Gen3
 	params->PcieRpPmSci[4] = 1;
+	params->PcieRpAcsEnabled[4] = 1;
+	params->PcieRpTransmitterHalfSwing[4] = 1;
 	params->PcieRpEnableCpm[4] = CONFIG(PCIEXP_CLK_PM);
 	params->PcieRpAspm[4] = aspm;
 	params->PcieRpL1Substates[4] = aspm_l1;
 
 	params->PcieRpMaxPayload[8]  = 1; // M.2 NGFF
 	params->PcieRpPmSci[8] = 1;
+	params->PcieRpAcsEnabled[8] = 1;
+	params->PcieRpTransmitterHalfSwing[8] = 1;
 	params->PcieRpEnableCpm[8] = CONFIG(PCIEXP_CLK_PM);
 	params->PcieRpAspm[8] = aspm;
 	params->PcieRpL1Substates[8] = aspm_l1;
 	
 	params->PcieRpMaxPayload[10]  = 1; // RTL8111 NIC
 	params->PcieRpPmSci[10] = 1;
+	params->PcieRpAcsEnabled[10] = 1;
+	params->PcieRpTransmitterHalfSwing[10] = 1;
 	params->PcieRpEnableCpm[10] = CONFIG(PCIEXP_CLK_PM);
 	params->PcieRpAspm[10] = aspm;
 	params->PcieRpL1Substates[10] = aspm_l1;
 
-
 	params->PcieRpMaxPayload[11]  = 1; // PCI-E x1 Gen3
 	params->PcieRpPmSci[11] = 1;
+	params->PcieRpAcsEnabled[11] = 1;
+	params->PcieRpTransmitterHalfSwing[11] = 1;
 	params->PcieRpEnableCpm[11] = CONFIG(PCIEXP_CLK_PM);
 	params->PcieRpAspm[11] = aspm;
 	params->PcieRpL1Substates[11] = aspm_l1;
