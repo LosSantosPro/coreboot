57315c9a9a27f9dd88e91e261c97d493bf8d9049
Alicja Michalska <ahplka19@gmail.com>
2024-07-25 20:12:56 +0200

    mb/erying/tgl: Disable CLKSRCs and ASPM
    Looks like ASPM is indeed impossible on this board after all :(

--- src/mainboard/erying/tgl/ramstage.c
+++ src/mainboard/erying/tgl/ramstage.c
@@ -17,31 +17,12 @@ struct chip_operations mainboard_ops = {
 void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 {
 
-	uint8_t aspm, aspm_l1;
-
-	/* ASPM L1 sub-states require CLKREQ, so CLK_PM should be enabled as well */
-	if (CONFIG(PCIEXP_L1_SUB_STATE) && CONFIG(PCIEXP_CLK_PM))
-		aspm_l1 = 2; // 2 - L1.1 and L1.2
-	else
-		aspm_l1 = 0;
-
-	if (CONFIG(PCIEXP_ASPM)) {
-		aspm = CONFIG(PCIEXP_L1_SUB_STATE) ? 3 : 1; // 3 - L0sL1, 1 - L0s
-	} else {
-		aspm = 0;
-		aspm_l1 = 0;
-	}
-
-
 	// PEG0 - Gen4 NVME
 	params->CpuPcieRpSlotImplemented[0] = 1;
 	params->CpuPcieRpAdvancedErrorReporting[0] = 1;
 	params->CpuPcieRpPeerToPeerMode[0] = 1;
 	params->CpuPcieRpAcsEnabled[0] = 1;
 	params->CpuPcieRpTransmitterHalfSwing[0] = 1;
-	params->CpuPcieRpEnableCpm[0] = CONFIG(PCIEXP_CLK_PM);
-	params->CpuPcieRpAspm[0] = aspm;
-	params->CpuPcieRpL1Substates[0] = aspm_l1;
 
 	// PEG1 - PCI-E x16
 	params->CpuPcieRpSlotImplemented[1] = 1;
@@ -49,49 +30,54 @@ void mainboard_silicon_init_params(FSP_S_CONFIG *params)
 	params->CpuPcieRpPeerToPeerMode[1] = 1;
 	params->CpuPcieRpAcsEnabled[1] = 1;
 	params->CpuPcieRpTransmitterHalfSwing[1] = 1;
-	params->CpuPcieRpEnableCpm[1] = CONFIG(PCIEXP_CLK_PM);
-	params->CpuPcieRpAspm[1] = aspm;
-	params->CpuPcieRpL1Substates[1] = aspm_l1;
-
-	// Power management
-	params->CpuPciePowerGating = CONFIG(PCIEXP_CLK_PM);
-	params->CpuPcieClockGating = CONFIG(PCIEXP_CLK_PM);
-	params->PchLegacyIoLowLatency = 1;
-	params->PchDmiAspmCtrl = 0;
 
 	// PCH RootPorts
 	params->PcieRpMaxPayload[4] = 2; // M.2 Gen3
-	params->PcieRpPmSci[4] = 1;
 	params->PcieRpAcsEnabled[4] = 1;
 	params->PcieRpTransmitterHalfSwing[4] = 1;
-	params->PcieRpEnableCpm[4] = CONFIG(PCIEXP_CLK_PM);
-	params->PcieRpAspm[4] = aspm;
-	params->PcieRpL1Substates[4] = aspm_l1;
 
 	params->PcieRpMaxPayload[8]  = 1; // M.2 NGFF
-	params->PcieRpPmSci[8] = 1;
 	params->PcieRpAcsEnabled[8] = 1;
 	params->PcieRpTransmitterHalfSwing[8] = 1;
-	params->PcieRpEnableCpm[8] = CONFIG(PCIEXP_CLK_PM);
-	params->PcieRpAspm[8] = aspm;
-	params->PcieRpL1Substates[8] = aspm_l1;
-	
+
 	params->PcieRpMaxPayload[10]  = 1; // RTL8111 NIC
-	params->PcieRpPmSci[10] = 1;
-	params->PcieRpAcsEnabled[10] = 1;
+	params->PcieRpLtrEnable[10] = 1;
 	params->PcieRpTransmitterHalfSwing[10] = 1;
-	params->PcieRpEnableCpm[10] = CONFIG(PCIEXP_CLK_PM);
-	params->PcieRpAspm[10] = aspm;
-	params->PcieRpL1Substates[10] = aspm_l1;
 
 	params->PcieRpMaxPayload[11]  = 1; // PCI-E x1 Gen3
-	params->PcieRpPmSci[11] = 1;
 	params->PcieRpAcsEnabled[11] = 1;
 	params->PcieRpTransmitterHalfSwing[11] = 1;
-	params->PcieRpEnableCpm[11] = CONFIG(PCIEXP_CLK_PM);
-	params->PcieRpAspm[11] = aspm;
-	params->PcieRpL1Substates[11] = aspm_l1;
+
+	// Power management: Force-disable ASPM
+	params->CpuPciePowerGating = 0;
+	params->CpuPcieClockGating = 0;
+	params->PchLegacyIoLowLatency = 1;
+	params->PchDmiAspmCtrl = 0;
+
+	params->CpuPcieRpEnableCpm[0] = 0;
+	params->CpuPcieRpAspm[0] = 0;
+	params->CpuPcieRpL1Substates[0] = 0;
+
+	params->CpuPcieRpEnableCpm[1] = 0;
+	params->CpuPcieRpAspm[1] = 0;
+	params->CpuPcieRpL1Substates[1] = 0;
+
+	params->PcieRpEnableCpm[4] = 0;
+	params->PcieRpAspm[4] = 0;
+	params->PcieRpL1Substates[4] = 0;
 	
+	params->PcieRpEnableCpm[8] = 0;
+	params->PcieRpAspm[8] = 0;
+	params->PcieRpL1Substates[8] = 0;
+
+	params->PcieRpEnableCpm[10] = 0;
+	params->PcieRpAspm[10] = 0;
+	params->PcieRpL1Substates[10] = 0;
+
+	params->PcieRpEnableCpm[11] = 0;
+	params->PcieRpAspm[11] = 0;
+	params->PcieRpL1Substates[11] = 0;
+
 	// FSP settings
 	params->PchUsbOverCurrentEnable = 0;
 	params->RC1pFreqEnable = 1;
